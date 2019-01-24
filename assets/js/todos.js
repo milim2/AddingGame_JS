11b354172152d97a7e32d00ad8b7eb52ae5a6452
milim2 <milimlee0@gmail.com>
2019-01-23 23:06:42 -0500

    todo list -start

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+
+// check off specific todos by clicking
+//$("li").click(function(){
+    // if li is gray
+    // turn it black
+    // console.log($(this).css("color"));
+    // if($(this).css("color") === "rgb(128, 128, 128)") {
+
+    //     $(this).css ({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     });
+    //     console.log("It is currently gray !");
+    // }
+
+    // else
+    // turn it gray
+
+    // $(this).css({
+    //     color: "grey",
+    //     textDecoration: "line-through"
+    // });
+    //$(this).toggleClass("completed");
+//});
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+

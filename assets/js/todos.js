5a494401d2fd97f8786041b5ebd7b9a681d03dc8
milim2 <milimlee0@gmail.com>
2019-01-24 23:42:20 -0500

    remove the lists

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -27,3 +27,11 @@ $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
+// Click on X to delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+

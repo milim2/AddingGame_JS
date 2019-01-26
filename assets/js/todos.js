08fb5bc535b79789308774d4046eb19c43f3ee47
milim2 <milimlee0@gmail.com>
2019-01-25 22:10:43 -0500

    add input into ul

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -28,10 +28,20 @@ $("ul").on("click", "li", function(){
 });
 
 // Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        // grabbing a new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>x</span>" + todoText + "</li>")
+    }
 });
 
